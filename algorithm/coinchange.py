f78d8ef549f8c184d239d396c37b658a30ac1fcd
pythonee <pythonee@gmail.com>
2012-03-26 22:49:43 +0800

    check solution for coins change

--- algorithm/coinchange.py
+++ algorithm/coinchange.py
@@ -14,7 +14,7 @@ def countMinCoinChange(cointypes, money):
     
     lastcoin = [0 for i in range(0, money+1)]
     usedcoin = [0 for i in range(0, money+1)]
-
+    
     for x in range(1, money+1):
         mincoins = x
         for coin in cointypes:
@@ -22,27 +22,41 @@ def countMinCoinChange(cointypes, money):
                 if(usedcoin[x - coin] + 1 <= mincoins):
                     mincoins = usedcoin[x - coin] + 1
                     lastcoin[x] = coin
-                    usedcoin[x] = mincoins                    
-    trackprint(lastcoin, money)
-    return usedcoin[money]
-    
-def trackprint(lastcoin, money):
-    if money == 0:
-        print ''
-        return
+                    usedcoin[x] = mincoins
+                    
+    if checksolution(lastcoin, money):
+        print 'money %d need at least %d coins [%s]' % (money,
+                                                        usedcoin[money],
+                                                        trackprint(lastcoin, money, [])
+                                                        )
     else:
-        print lastcoin[money],
-        trackprint(lastcoin, money-lastcoin[money])
+        print 'money %d have no solution' % money
+
+def checksolution(lastcoin, money):
+    while money>0:
+        if lastcoin[money] == 0:
+            return False
+        money = money - lastcoin[money]
+    return True    
+    
+def trackprint(lastcoin, money, coins=[]):
+    if money == 0:
+        return ','.join(str(i) for i in coins)
+
+    else:
+        coins.append(lastcoin[money])
+        return trackprint(lastcoin, money-lastcoin[money], coins)
         
 if __name__ == '__main__':
     #cointypes = [ 50, 10, 1, 5, 25 ]
     #print countCoinChange_1(cointypes, 100)
     
-    cointypes = [25, 21, 10, 5, 1]
-    
-    print countMinCoinChange(cointypes, 16)
-    print countMinCoinChange(cointypes, 15)
-    print countMinCoinChange(cointypes, 46)
-    print countMinCoinChange(cointypes, 62)
-    print countMinCoinChange(cointypes, 63)
-    print countMinCoinChange(cointypes, 100)
+    cointypes = [25, 20, 10, 5, 2]
+    countMinCoinChange(cointypes, 0)
+    countMinCoinChange(cointypes, 16)
+    countMinCoinChange(cointypes, 15)
+    countMinCoinChange(cointypes, 11)
+    countMinCoinChange(cointypes, 49)
+    countMinCoinChange(cointypes, 62)
+    countMinCoinChange(cointypes, 63)
+    countMinCoinChange(cointypes, 100)
