4249181ffbcc0a0092fa4aaecd3fece4ab6429f7
pythonee <pythonee@gmail.com>
2012-03-30 10:14:56 +0800

    re-describe statement

--- algorithm/benchmark.py
+++ algorithm/benchmark.py
@@ -4,4 +4,4 @@ def rt(func, *argc): # running time
     start = time.clock()
     func(*argc)
     end = time.clock()
-    print '%s method need last %f seconds' %(func.__name__, end-start)
+    print '%s method takes %f seconds' %(func.__name__, end-start)
