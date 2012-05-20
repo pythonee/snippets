30fad9f2bc797c7c1ce075c1ac1fded018d01654
pythonee <pythonee@gmail.com>
2012-05-20 14:02:08 +0800

    add argparse

--- tools/img_merge.py
+++ tools/img_merge.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 import random
 import PIL.Image as Image
-import os,sys
-from itertools import chain
+import os
+import argparse
 
 suffixs = ['.jpg','.png']
+
+
+def args_parse():
+    parser = argparse.ArgumentParser(description='image merge')
+    parser.add_argument('-path',nargs='?',dest='path',default=os.getcwd(),type=str)
+    parser.add_argument('-uw',nargs='?',dest='uw',default=120,type=int)
+    parser.add_argument('-uh',nargs='?',dest='uh',default=120,type=int)
+    parser.add_argument('-cn',nargs='?',dest='cn',default=5,type=int)
+    parser.add_argument('-margin',nargs='?',dest='margin',default=4,type=int)
+    parser.add_argument('-bg',nargs='?',dest='bg',choices=['w','b'],default='w',type=str)
+
+    return parser.parse_args()
 
 def walk_dir(path):
     selected_files = []
@@ -12,20 +24,11 @@ def walk_dir(path):
         for f in files:
             path_to_file = os.path.join(root, f)
             filename, ext = os.path.splitext(f)
-            
+
             if ext.lower() in suffixs:
                 selected_files.append(path_to_file)
 
     return selected_files
-
-def get_base_path():
-    base_path = ''
-    if len(sys.argv) >= 2:
-        base_path = sys.argv[1]
-    else:
-        base_path = os.getcwd()
-    return base_path
-
 
 def shuff_file(files):
     for i in range(0,len(files)):
@@ -35,48 +38,42 @@ def shuff_file(files):
         files[x] = temp;
     return files
 
-def merge(files):
-    UNIT_WIDTH = 120 #每张图片宽度
-    UNIT_HEIGHT = 120 #每张图片高度
-    IMG_OF_ROW = 6 # 每行图片数
+def merge(files, uw, uh, cn, margin, bg):
 
-    COLS = 4  # 间隙
-    
+    num_of_img = len(files)
 
-    NUM_OF_IMG = len(files)
-    
-    
-    NUM_OF_ROW = NUM_OF_IMG / IMG_OF_ROW
-    if NUM_OF_IMG > NUM_OF_ROW * IMG_OF_ROW:
-        NUM_OF_ROW += 1
-        
-    WIDTH_OF_TARGET = UNIT_WIDTH * IMG_OF_ROW + (IMG_OF_ROW+1)*COLS
-    HEIGHT_OF_TARGET = UNIT_HEIGHT * NUM_OF_ROW + (NUM_OF_ROW+1)*COLS
-    QUALITY_VALUE = 100
-       
-    target = Image.new("RGBA",(WIDTH_OF_TARGET,HEIGHT_OF_TARGET))
-    
-    STEP_WIDTH = UNIT_WIDTH + COLS
-    STEP_HEIGHT = UNIT_HEIGHT + COLS
 
-    left = COLS
-    top = COLS
-    
+    rn = num_of_img / cn
+    if num_of_img > rn * cn:
+        rn += 1
+
+    width_of_target = uw * cn + (cn+1)*margin
+    height_of_target = uh * rn + (rn+1)*margin
+    quality_value = 100
+
+    target = Image.new("RGBA",(width_of_target,height_of_target),bg)
+
+    step_width = uw + margin
+    step_height = uh + margin
+
+    left = margin
+    top = margin
+
     count = 0
     for f in files:
         img = Image.open(f);
-        img = img.resize((UNIT_WIDTH, UNIT_HEIGHT), Image.ANTIALIAS)
+        img = img.resize((uw ,uh), Image.ANTIALIAS)
 
         target.paste(img, (left, top))
-        left += STEP_WIDTH
+        left += step_width
 
         count += 1
-        if count == IMG_OF_ROW:
+        if count == cn:
             count = 0
-            left = COLS
-            top += STEP_HEIGHT
-            
-    target.save("result.jpg", quality=QUALITY_VALUE)
+            left = margin
+            top += step_height
+
+    target.save("result.jpg", quality=quality_value)
     print 'done'
 
 if __name__ == '__main__':
@@ -84,12 +81,24 @@ if __name__ == '__main__':
         os.remove(os.path.join(os.getcwd(),'result.jpg'))
     except OSError:
         pass
-    base_path = get_base_path()
+    args = args_parse()
+    base_path = args.path # 图片所在路径
+    uw = args.uw    # unit_width 小图片宽度
+    uh = args.uh    # unit_height 小图片高度
+    cn = args.cn    # 每行列数
+    margin = args.margin    # 行间距，也是列间距
+
+    bg = args.bg    # 背景颜色
+    if bg=='w':
+        bg=(255,255,255,0)
+    else:
+        bg=(0,0,0,0)
+
     files = walk_dir(base_path)
     files = shuff_file(files)
-    
-    merge(files)
 
-        
-    
-        
+    merge(files,uw,uh,cn,margin,bg)
+
+
+
+
