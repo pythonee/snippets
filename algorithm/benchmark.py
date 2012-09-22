20ffa04552cad23e4956fe6f33d13c3913551d78
pythonee <pythonee@gmail.com>
2012-09-22 10:26:26 +0800

    rm python algorithm implementation

--- algorithm/benchmark.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def rt(func, *argv): # running time
-    start = time.clock()
-    func(*argv)
-    end = time.clock()
-    print '%s method takes %f seconds' %(func.__name__, end-start)
